8e2fb5b5a10cee3a52a9396d9f99c8207a149674
Kroquetita <kroquetita@gmail.com>
2024-03-07 08:06:49 +0100

    refresh and login y register

--- blog/users/api/views.py
+++ blog/users/api/views.py
@@ -3,8 +3,15 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from users.models import User
+from users.api.serializers import UserRegisterSerializer
 
 class RegisterView(APIView):
     def post(self, request):
-        print('registrando usuario...')
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+        serializer = UserRegisterSerializer(data=request.data)
+        
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
